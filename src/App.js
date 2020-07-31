0aea25858079d44b49ae283181e055d19ebdd978
sirbaconsalot <donavynhaley@gmail.com>
2020-07-30 21:31:58 -0700

    Diffrent styling that is still be worked on

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React, {useState, useEffect} from "react";
 import "./App.css";
-import Head from './components/Head.js'
-import Img from './components/Img.js'
-import Description from './components/Description.js'
-import Footer from './components/Footer.js'
+import downloadLogo from "./assets/images/download.png"
 
-// API Key 4LTBH2pc9kXTBhZhWFUK7oGJoUklB1UuPhhD7aOF
+// API Key gWUHFoXqRrldO8cHpW9aU55FjssJ0CPzTOhQp82a
 // Example Use: https://api.nasa.gov/planetary/apod?api_key=4LTBH2pc9kXTBhZhWFUK7oGJoUklB1UuPhhD7aOF
 const axios = require('axios');
 
@@ -13,20 +10,44 @@ function App() {
   const [astroPic, setAstroPic] = useState({});
 
   useEffect(() => {
-    axios
-      .get("https://api.nasa.gov/planetary/apod?api_key=4LTBH2pc9kXTBhZhWFUK7oGJoUklB1UuPhhD7aOF")
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=gWUHFoXqRrldO8cHpW9aU55FjssJ0CPzTOhQp82a&date=2020-07-30")
       .then(response =>{
+        console.log(response);
         setAstroPic(response.data)
-        console.log(response.data);
-      });
+      })
+      .catch(error =>{
+        console.log("Error: ", error);
+      })
   }, []);
 
   return (
     <div className="App">
-      <Head date={astroPic.date} title={astroPic.title} />
-      <Img imgSdSrc={astroPic.url} imgHdSrc={astroPic.hdurl} copyright={astroPic.copyright}/>
-      <Description explanation={astroPic.explanation}/>
-      <Footer />
+      <header>
+        <h1>NASA Astronomy Picture of the Day</h1>
+      </header>
+      <div className="main">
+        <section className="left-section">
+          <div className="img-header">
+            <h2>{astroPic.title}</h2>
+            <h3>{astroPic.date}</h3>
+          </div>
+          <div className="img-container" style={{
+            backgroundImage: `url(${astroPic.url})`,
+            height: `${astroPic.url.natualHight}`
+          }} >
+            <div className="sd-hd">
+              <button>SD</button>
+              <button>HD</button>
+              <img src={downloadLogo} alt="Download icon" id="download-icon" width="30px" height="30px"/>
+              <p>Copyright: {astroPic.copyright}</p>
+            </div>
+          </div>
+        </section>
+        <section className="right-section">
+          <h2>Description</h2>
+          <p>{astroPic.explanation}</p>
+        </section>
+      </div>
     </div>
   );
 }
